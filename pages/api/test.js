0a77d7fde9862832938a0c2da71e1102bae21c84
adamszmyd <szmydadam@gmail.com>
2023-11-19 10:35:28 +0100

    AI Devs

--- pages/api/test.js
+++ pages/api/test.js
@@ -1,3 +1,43 @@
-export default function handler(req, res) {
-    res.status(200).json({ message: 'Hello from Next.js! ' + process.env.OPENAI_API_KEY })
+import 'dotenv/config';
+import OpenAI from 'openai';
+const openai = new OpenAI();
+
+// curl -X POST http://localhost:3000/api/test    -H "Content-Type: application/json"   -d '{"question":"What is your name?"}'
+
+export default async function handler(req, res) {
+
+    console.log('running...', req.body);
+    if(typeof req.body === 'object') {
+        const question = req.body.question;
+        if(typeof question === 'string') {
+
+            console.log('asking GPT', question);
+            
+            const answer =  (
+                await openai.chat.completions.create({
+                    model: 'gpt-3.5-turbo',
+                    messages: [
+                        {
+                            role: 'user',
+                            content: question,
+                        },
+                    ],
+                })
+            ).choices[0].message.content ?? ''
+
+            console.log('returning answer', answer);
+
+            res.status(200).json({ answer: answer })
+
+            
+        } else {
+
+         res.status(200).json({ message: "Nothing to do" })
+        }
+    } else {
+
+        res.status(200).json({ message: "Nothing to do" })
+    }
+    
+
   }
